b2104b88371c109e3673d57ca8b7b64375d4fdcb
Quintium <yzmailer@gmail.com>
2022-12-04 13:46:32 +0100

    Comment two-fold repetition

--- src/AI.cpp
+++ src/AI.cpp
@@ -156,6 +156,7 @@ int AI::search(int alpha, int beta, int depth, int plyFromRoot)
 	// increase nodes searched
 	nodes++;
 	 
+	// mark a two-fold repetition as a draw -> not completely safe as some positions get misjudged, but better in general
 	if (board->checkRepetition() && plyFromRoot >= 1)
 	{
 		return DRAW_SCORE;
