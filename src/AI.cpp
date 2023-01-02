ed1577ad1a4c371c328e52be9ca080c6d18bebc3
Quintium <yzmailer@gmail.com>
2023-01-02 22:47:56 +0100

    Remove endgame restriction

--- src/AI.cpp
+++ src/AI.cpp
@@ -214,7 +214,7 @@ int AI::search(int alpha, int beta, int depth, int plyFromRoot, bool nullMove)
 	}
 
 	// evaluate null move
-	if (!board->getCheck() && !nullMove && depth > 3 && evaluation->getEndgameWeight() < 0.5)
+	if (!board->getCheck() && !nullMove && depth > 3)
 	{
 		board->makeMove(Move::nullmove());
 		int nullEval = -search(-beta, -alpha, depth - 4, plyFromRoot + 1, true);
