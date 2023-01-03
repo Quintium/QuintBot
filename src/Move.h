62d376034af6d64ee9329adbb03365d1b6250ace
Quintium <yzmailer@gmail.com>
2023-01-03 18:12:56 +0100

    Split Move header into two files

--- src/Move.h
+++ src/Move.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "Piece.h"
 
 // class for squares in the board
 class Square
@@ -87,140 +88,24 @@ public:
 struct Move
 {
 	// all move properties
-	int from;
-	int to;
-	int piece;
-	int cPiece;
-	bool enPassant;
-	bool castling;
-	int promotion;
-	int score;
+	int from; // square the piece moved from
+	int to; // square the piece moved to
+	int piece; // piece that moved
+	int cPiece; // piece that was captured
+	bool enPassant; // if move was en passant
+	bool castling; // if move was castling
+	int promotion; // piece that was promoted to
+	int score; // score of move during move ordering
 
-	// checking if two moves are the same by checking from and to squares and promotion piece
-	bool operator==(Move move)
-	{
-		return (move.from == from) && (move.to == to) && (move.promotion == promotion);
-	}
+	// comparisons and notation
+	bool operator==(Move move);
+	std::string getNotation();
 
-	// return notation of move e.g. "a1f3" or "c2c1q" 
-	std::string getNotation()
-	{
-		// handle nullmoves
-		if (Move::isNull(*this))
-		{
-			return "0000";
-		}
+	// loading moves
+	static Move loadFromSquares(int from, int to, int* piecesMB);
+	static Move loadFromNotation(std::string notation, int* piecesMB);
 
-		// concatenate from and to squares as base of move
-		std::string base = Square::toString(from) + Square::toString(to);
-
-		// if there's a promotion, specify the promotion piece
-		if (promotion != -1)
-		{
-			switch (Piece::typeOf(promotion))
-			{
-			case QUEEN:
-				base += "q";
-				break;
-			case ROOK:
-				base += "r";
-				break;
-			case BISHOP:
-				base += "b";
-				break;
-			case KNIGHT:
-				base += "n";
-				break;
-			}
-		}
-
-		return base;
-	}
-
-	// load move from source and target squares
-	static Move loadFromSquares(int from, int to, int* piecesMB)
-	{
-		// move properties
-		int movedPiece = piecesMB[from];
-		int takenPiece = piecesMB[to];
-		int promotionPiece = EMPTY;
-		bool isEnPassant = false;
-		bool isCastling = false;
-
-		// get piece color and type
-		int pieceColor = Piece::colorOf(movedPiece);
-		int pieceType = Piece::typeOf(movedPiece);
-
-		// en passant move properties
-		if ((from - to) % 8 != 0 && piecesMB[to] == EMPTY && pieceType == PAWN)
-		{
-			takenPiece = !pieceColor + PAWN;
-			isEnPassant = true;
-		}
-
-		// castling move properties
-		if (std::abs(from - to) == 2 && pieceType == KING)
-		{
-			isCastling = true;
-		}
-
-		// promotion move properties
-		if ((Square::rankOf(to) == pieceColor * 7) && (pieceType == PAWN))
-		{
-			promotionPiece = QUEEN + pieceColor;
-		}
-
-		// return move based on properties
-		Move move = { from, to, movedPiece, takenPiece, isEnPassant, isCastling, promotionPiece, 0 };
-		return move;
-	}
-
-	// load move from long algebraic notation
-	static Move loadFromNotation(std::string notation, int* piecesMB)
-	{
-		// handle nullmoves
-		if (notation == "0000")
-		{
-			return Move::nullmove();
-		}
-
-		int from = Square::fromString(notation.substr(0, 2));
-		int to = Square::fromString(notation.substr(2, 4));
-		Move move = Move::loadFromSquares(from, to, piecesMB);
-
-		int pieceColor = Piece::colorOf(piecesMB[from]);
-		if (notation.size() == 5)
-		{
-			switch (notation[4])
-			{
-			case 'q':
-				move.promotion = QUEEN + pieceColor;
-				break;
-			case 'r':
-				move.promotion = ROOK + pieceColor;
-				break;
-			case 'b':
-				move.promotion = BISHOP + pieceColor;
-				break;
-			case 'n':
-				move.promotion = KNIGHT + pieceColor;
-				break;
-			}
-		}
-
-		return move;
-	}
-
-	// return a null move
-	static Move nullmove()
-	{
-		Move newMove = { EMPTY, EMPTY, EMPTY, EMPTY, false, false, EMPTY, 0 };
-		return newMove;
-	}
-
-	// check if move is valid
-	static bool isNull(Move move)
-	{
-		return move == Move::nullmove();
-	}
+	// null moves
+	static Move nullmove();
+	static bool isNull(Move move);
 };
